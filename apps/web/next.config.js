66e647d3ae40ce8c313ced02411d5e67af773537
Boris Avgustinovich <74373178+vairaden@users.noreply.github.com>
2022-09-22 13:53:15 +0300

    UPDATE: db in container set up

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+const path = require("path");
+
 const nextConfig = {
   reactStrictMode: true,
+  output: "standalone",
+  experimental: {
+    outputFileTracingRoot: path.join(__dirname, "../../"),
+  },
   swcMinify: true,
   images: {
     domains: ["localhost"],
